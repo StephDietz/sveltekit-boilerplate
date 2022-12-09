c8534e881a0897277d90bd4033e755ea758dbe6e
StephDietz <steph.dietz@vercel.com>
2022-12-09 13:53:39 -0600

    add vercel page

---
+++ src/routes/vercel/+page.svelte
@@ -0,0 +1,45 @@
+<div class="contianer">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Vercel_logo_black.svg/2560px-Vercel_logo_black.svg.png" alt="Vercel Logo" width="120" />
+    <div style="font-size: 24px; font-weight: 700; margin: 20px 0 20px 0;">
+        SvelteKit on Vercel
+    </div> 
+    <div>
+        Vercel is a cloud platform for deploying and hosting web applications. Using Vercel in conjunction with SvelteKit creates a dream stack, offering several improvements:
+
+        <ul>
+            <li>
+                Vercel provides a zero-configuration platform for deploying and hosting SvelteKit apps, making it easy to get your app up and running quickly.
+            </li>
+            <li>
+                Vercel recently launched [Edge Functions](https://vercel.com/features/edge-functions), which allow you to run JavaScript code on their globally-distributed edge network. SvelteKit supports these Edge Functions, meaning you can serve your users dynamically-rendered pages at the same speed you would serve static files from a CDN, drastically improving the performance and reliability of your SvelteKit app.
+            </li>
+            <li>
+                Vercel offers Vercel Analytics in the dashboard to help you understand the performance of your application based on real visitor data. With the Vercel Analytics API, you can now use Vercel Analytics with SvelteKit.
+            </li>
+            <li>
+                Vercel provides built-in support for server-side rendering (SSR) and static site generation (SSG), which can improve the performance and SEO of your SvelteKit app.
+            </li>
+            <li>
+                Vercel offers seamless integrations with popular development tools and services, such as GitHub, GitLab, and Visual Studio Code, making it easy to integrate your SvelteKit app into your existing workflow.
+            </li>
+            <li>
+                Vercel provides a powerful, intuitive interface for managing and monitoring your SvelteKit app, allowing you to see how your app is performing and make updates and changes as needed.
+            </li>
+        </ul>
+        Overall, Vercel can provide a number of benefits when used with SvelteKit, making it easier to deploy, host, and manage your SvelteKit app. Whether you're a small team building a simple web app or a large organization with complex, mission-critical applications, Vercel can help you get the most out of your SvelteKit app.
+    </div>
+    <div style="margin: 20px 0 20px 0;">
+        Check out Vercel's <a href="/">SvelteKit 1.0 on Vercel</a> blog to learn more, or take the free <a href="https://beginnersveltekit.com">Beginner SvelteKit</a> course! 
+    </div>
+
+</div>
+
+<style>
+.contianer {
+    display: flex;
+	max-width: 48rem;
+	flex: 0.6;
+	flex-direction: column;
+	margin: 0 auto;
+}
+</style>
