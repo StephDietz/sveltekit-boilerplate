87f22ea3d00b67b40b75d95e5ed29b95468b312a
StephDietz <steph.dietz@vercel.com>
2022-12-13 10:08:14 -0600

    remove vitals

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,6 @@
 <script>
 	import Header from './Header.svelte';
 	import './styles.css';
-	import { webVitals } from '$lib/vitals';
-	import { browser} from '$app/environment';
-	import { page } from '$app/stores';
-	let analyticsId = import.meta.env.VERCEL_ANALYTICS_ID;
-	$: if (browser && analyticsId) {
-		webVitals({
-		path: $page.url.pathname,
-		params: $page.params,
-		analyticsId
-		})
-	}
 </script>
 
 <div class="app">
--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()]
-	// define: {
-	// 	'import.meta.env.VERCEL_ANALYTICS_ID': JSON.stringify(process.env.VERCEL_ANALYTICS_ID)
-	// }
 };
 
 export default config;
